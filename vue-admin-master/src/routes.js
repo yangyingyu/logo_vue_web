11a926e8f8cd7dafca90ea2e180ab9461ebabe33
yangjun <yangjun@123.com>
2019-03-01 09:17:27 +0800

    2019/3/1

--- vue-admin-master/src/routes.js
+++ vue-admin-master/src/routes.js
@@ -9,6 +9,8 @@ import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
 import Page6 from './views/nav3/Page6.vue'
 import echarts from './views/charts/echarts.vue'
+import Brand from './views/product/Brand.vue'
+import ProductType from './views/product/ProductType.vue'
 
 let routes = [
     {
@@ -23,46 +25,47 @@ let routes = [
         name: '',
         hidden: true
     },
-    //{ path: '/main', component: Main },
     {
         path: '/',
         component: Home,
-        name: '导航一',
-        iconCls: 'el-icon-message',//图标样式class
-        children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/table', component: Table, name: 'Table' },
-            { path: '/form', component: Form, name: 'Form' },
-            { path: '/user', component: user, name: '列表' },
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '导航二',
-        iconCls: 'fa fa-id-card-o',
-        children: [
-            { path: '/page4', component: Page4, name: '页面4' },
-            { path: '/page5', component: Page5, name: '页面5' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: '',
-        iconCls: 'fa fa-address-card',
+        name: '首页',
         leaf: true,//只有一个节点
-        children: [
-            { path: '/page6', component: Page6, name: '导航三' }
-        ]
-    },
-    {
-        path: '/',
-        component: Home,
-        name: 'Charts',
         iconCls: 'fa fa-bar-chart',
         children: [
-            { path: '/echarts', component: echarts, name: 'echarts' }
+            { path: '/echarts', component: echarts, name: '首页' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '商品模块',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/productBrand', component: Brand, name: '商品品牌' },
+            {path: '/productType', component: ProductType, name: '商品类型' }
+            /*,
+            { path: '/productType', component: ProductType, name: '商品类型' },
+            { path: '/productViewProperties', component: user, name: '属性管理' },
+            { path: '/product', component: Product, name: '商品管理' },*/
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '系统管理',
+        iconCls: 'fa fa-id-card-o',
+        children: [
+            { path: '/page4', component: Page4, name: '员工管理' },
+            { path: '/page5', component: Page5, name: '部门管理' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '营销管理',
+        iconCls: 'fa fa-address-card',
+        children: [
+            { path: '/page6', component: Page6, name: '营销活动管理' }
         ]
     },
     {
